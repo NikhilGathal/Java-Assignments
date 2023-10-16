d6bb819b8bc9fb8c4d8336a9a6833f67012ec799
NikhilGathal <nikhilgathal@gmail.com>
2023-10-16 09:27:13 +0530

    Some modification

--- Java-1.java
+++ Java-1.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Assignment1 {
+class Assignment1 {
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>();
--- Java-3.java
+++ Java-3.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Assignment3 {
+class Assignment3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
@@ -18,22 +18,17 @@ public class Assignment3 {
 
         boolean[] alphabetPresent = new boolean[26];
 
-        // Convert the input to lowercase for case-insensitive checking
-        // input = input.toLowerCase();
-
         input = input.toUpperCase();
 
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
 
-            // Check if the character is an alphabet letter
             if ('A' <= ch && ch <= 'Z') {
-                // Set the corresponding entry in the array to true
+
                 alphabetPresent[ch - 'A'] = true;
             }
         }
 
-        // Check if all alphabet letters are present
         for (boolean present : alphabetPresent) {
             if (!present) {
                 return false;
